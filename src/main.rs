c7bfde09a7d69774e3aa67095d445ff5a54a2832
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-02-16 23:13:32 +0200

    add message logger

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@
 
 use crossterm::event::{read, Event, KeyCode};
 use crossterm::{cursor, event, execute, style, terminal};
-use std::{io, process};
+use std::{fmt, io, process};
 
+#[derive(Debug)]
 enum Direction {
     Up,
     Down,
     Left,
     Right,
+}
+
+impl fmt::Display for Direction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+        // or, alternatively:
+        // fmt::Debug::fmt(self, f)
+    }
 }
 
 enum Action {
@@ -41,25 +50,21 @@ fn main() {
     // Drawborders
     execute!(out, style::Print(BORDERS.join("\n\r")),);
 
+    let msg_position = cursor::MoveTo(2, 18);
+
     loop {
         draw_grid(grid);
 
         let action = get_action();
 
         match action {
-            Action::Exit => {
-                execute!(
-                    io::stdout(),
-                    cursor::MoveTo(2, 18),
-                    style::Print("Good bye!")
-                );
-                break;
-            }
-            Action::Navigate(direction) => {}
+            Action::Exit => break,
+            Action::Navigate(direction) => message(direction.to_string()),
             Action::Ignore => {}
         }
     }
 
+		message("Good bye!");
     execute!(out, cursor::Show);
     terminal::disable_raw_mode();
     process::exit(0);
@@ -67,12 +72,21 @@ fn main() {
 
 fn mutate_grid(grid: [[u16; 4]; 4], direction: Direction) {}
 
+fn message<S: Into<String>>(msg: S) {
+    execute!(
+        io::stdout(),
+        cursor::MoveTo(2, 18),
+        terminal::Clear(terminal::ClearType::CurrentLine),
+        style::Print(msg.into())
+    );
+}
+
 fn draw_grid(grid: [[u16; 4]; 4]) {
     for i in 0..grid.len() {
         let line = grid[i];
         for j in 0..line.len() {
             let cell = line[j];
-						let cell_len = cell.to_string().len() as u16;
+            let cell_len = cell.to_string().len() as u16;
             let x = 4 + j as u16 * 10 + (4 - cell_len) / 2;
             let y = 2 + i as u16 * 4;
             execute!(io::stdout(), cursor::MoveTo(x, y), style::Print(cell));
