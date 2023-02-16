598014d8e2218ac50d19144b702a0022a2529bcb
Andrew Luca <1881266+iamandrewluca@users.noreply.github.com>
2023-02-16 22:50:25 +0200

    draw borders and numbers

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use crossterm::event::{read, Event, KeyCode};
 use crossterm::{cursor, event, execute, style, terminal};
-use std::io::stdout;
+use std::{io, process};
 
 enum Direction {
     Up,
@@ -18,7 +18,8 @@ enum Action {
 }
 
 fn main() {
-    terminal::enable_raw_mode().unwrap();
+    terminal::enable_raw_mode();
+
     let mut grid = [
         [0, 2, 64, 8],
         [32, 14, 0, 0],
@@ -26,36 +27,55 @@ fn main() {
         [0, 2048, 0, 0],
     ];
 
-    let mut x = 0;
-    let y = 0;
+    let mut out = io::stdout();
 
-    let mut out = stdout();
-    let clear_screen = terminal::Clear(terminal::ClearType::All);
-    let goto_corner = cursor::MoveTo(0, 0);
+    execute!(out, cursor::Hide);
+
+    // Clear screen
+    execute!(
+        out,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::MoveTo(0, 0)
+    );
+
+    // Drawborders
+    execute!(out, style::Print(BORDERS.join("\n\r")),);
 
     loop {
-        execute!(out, clear_screen, goto_corner).unwrap();
-
-        draw_board(grid);
+        draw_grid(grid);
 
         let action = get_action();
 
         match action {
-            Action::Exit => break,
+            Action::Exit => {
+                execute!(
+                    io::stdout(),
+                    cursor::MoveTo(2, 18),
+                    style::Print("Good bye!")
+                );
+                break;
+            }
             Action::Navigate(direction) => {}
             Action::Ignore => {}
         }
     }
+
+    execute!(out, cursor::Show);
+    terminal::disable_raw_mode();
+    process::exit(0);
 }
 
 fn mutate_grid(grid: [[u16; 4]; 4], direction: Direction) {}
 
-fn draw_board(grid: [[u16; 4]; 4]) {
-    for line in grid {
-        for cell in line {
-            print!("{cell: <6}")
+fn draw_grid(grid: [[u16; 4]; 4]) {
+    for i in 0..grid.len() {
+        let line = grid[i];
+        for j in 0..line.len() {
+            let cell = line[j];
+            let x = 4 + j as u16 * 10;
+            let y = 2 + i as u16 * 4;
+            execute!(io::stdout(), cursor::MoveTo(x, y), style::Print(cell));
         }
-        execute!(stdout(), cursor::MoveToNextLine(2)).unwrap();
     }
 }
 
@@ -74,3 +94,24 @@ fn get_action() -> Action {
         _ => Action::Ignore,
     }
 }
+
+// Yes, but it looks good
+const BORDERS: &'static [&'static str] = &[
+    "[][][][][][][][][][][][][][][][][][][][][]",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[][][][][][][][][][][][][][][][][][][][][]",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[][][][][][][][][][][][][][][][][][][][][]",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[][][][][][][][][][][][][][][][][][][][][]",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[]        []        []        []        []",
+    "[][][][][][][][][][][][][][][][][][][][][]",
+];
